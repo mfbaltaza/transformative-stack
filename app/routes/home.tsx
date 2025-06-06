399a8e1589c08e50698acd61ba75ff0af25b94a2
Juan Araujo <araujogreydiego@gmail.com>
2025-06-06 16:02:20 -0400

    feat: adds random error boundary

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,6 +1,7 @@
 import type { Route } from "./+types/home";
 import Hero from "../components/Hero";
 import Footer from "../components/Footer";
+import { isRouteErrorResponse } from "react-router";
 
 export function meta({}: Route.MetaArgs) {
 	return [
@@ -29,5 +30,34 @@ export default function Home({ loaderData }: Route.ComponentProps) {
 			<Hero mocks_enabled={typeof loaderData === "object"} />
 			<Footer />
 		</div>
+	);
+}
+
+export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
+	let message = "Oops!";
+	let details = "An unexpected error occurred.";
+	let stack: string | undefined;
+
+	if (isRouteErrorResponse(error)) {
+		message = error.status === 404 ? "404" : "Error";
+		details =
+			error.status === 404
+				? "The requested page could not be found."
+				: error.statusText || details;
+	} else if (import.meta.env.DEV && error && error instanceof Error) {
+		details = error.message;
+		stack = error.stack;
+	}
+
+	return (
+		<main className="pt-16 p-4 container mx-auto">
+			<h1>{message}</h1>
+			<p>{details}</p>
+			{stack && (
+				<pre className="w-full p-4 overflow-x-auto">
+					<code>{stack}</code>
+				</pre>
+			)}
+		</main>
 	);
 }
